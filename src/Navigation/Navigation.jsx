c8e3384c6455e92b6f7381f1a54227714b46df13
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-03-28 19:18:56 +0300

    adding Context and Error Page

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage/HomePage';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
 function Navigation() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route component={ErrorPage} />
       </Switch>
     </BrowserRouter>
   );
 }
-
 export default Navigation;
