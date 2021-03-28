da1cf614cf7a8a5b3f6f077c67469d6052fc32dd
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-03-28 20:21:46 +0300

    adding Nav Menu and About Page

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage/HomePage';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import AboutPage from '../pages/AboutPage/AboutPage';
 
 function Navigation() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
         <Route component={ErrorPage} />
       </Switch>
     </BrowserRouter>
